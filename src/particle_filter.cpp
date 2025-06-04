0c7fa335c61339a64711d623e1f57592eda7a7c3
Hyunji Kim <khhj031@gmail.com>
2025-06-03 23:25:17 -0400

    [Add] Added motionUpdate function

--- src/particle_filter.cpp
+++ src/particle_filter.cpp
@@ -12,7 +12,7 @@ struct Particle {
 
 class ParticleFilter {
 public:
-    ParticleFilter(int num_particles) : gen{rd()}, num_particles(num_particles) {
+    ParticleFilter(int num_particles) : gen{rd()}, num_particles(num_particles), parameters{} {
         particles.resize(num_particles);
     }
 
@@ -31,10 +31,34 @@ public:
         }
     }
 
+    // Helper function to generate motion based noise
+    double motionNoise(double v, double w, const std::vector<double> p) {
+        return sqrt(p[0] * v * v + p[1] * w * w);
+    } 
+
+    // 2. Motion update (predict)
+    void motionUpdate(double delta_t, double velocity, double yaw) {
+        std::normal_distribution<double> x_noise_dist{0, motionNoise(velocity, yaw, parameters)};
+        std::normal_distribution<double> y_noise_dist{0, motionNoise(velocity, yaw, parameters)};
+        std::normal_distribution<double> theta_noise_dist{0, motionNoise(velocity, yaw, parameters)};
+
+        for (auto& p : particles) {
+            p.x += (velocity /yaw) * (-1 * sin(p.theta) + sin(p.theta + yaw * delta_t));
+            p.y += (velocity /yaw) * (cos(p.theta) - cos(p.theta + yaw * delta_t));
+            p.theta += yaw * delta_t;
+            
+            // Add random noise
+            p.x += x_noise_dist(gen);
+            p.y += y_noise_dist(gen);
+            p.theta += theta_noise_dist(gen);
+        }
+    }
+
 private:
     std::vector<Particle> particles;
     int num_particles;
     // Random number generator with random seed
     std::random_device rd{};
     std::mt19937 gen;
+    std::vector<double> parameters;
 };
